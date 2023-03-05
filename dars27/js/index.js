85c4f755d45cf36219a94adee293ce7a9de319ed
Doston3300 <113879352+Doston3300@users.noreply.github.com>
2023-03-05 15:22:07 +0500

    Add files via upload

---
+++ dars27/js/index.js
@@ -0,0 +1,181 @@
+// '' const userJob = prompt('Kasbingiz');
+//  if (userJob === 'student') {
+//      console.log('yaxwi uqing')
+//  } else {
+//      console.log('yaxwi ishlang')
+//  }
+
+
+
+
+// const myNum = +prompt('Raqam kiriting')
+// if (myNum % 2 === 0) {
+//     console.log('juft')
+// } else {
+//     console.log('toq son')
+// }
+
+
+
+
+
+// const num1 = +prompt('Birinchi sonni kiriting');
+// const num2 = +prompt('Birinchi sonni kiriting');
+// if (num1 > num2) {
+//     console.log('Birinchhi son katta')
+// } else {
+//     console.log('ikkinchi son katta')
+// }
+
+
+
+
+// const num1 = +prompt('sonni kiriting');
+// const num2 = +prompt('sonni kiriting');
+// if (num1 >= num2) {
+//     console.log(`Birinchhi son katta ${num1-num2} nisbattta katta`)
+// } else if (num1 === num2)
+//     console.log(`teng ${num1===num2}`)
+// else {
+//     console.log(`ikkinchi son katta ${num1-num2} nisbatta kichik`)
+// }
+
+
+
+
+
+
+// const myName = +prompt('son kiriting')
+// const yosh = 65
+// if (yosh <= myName) {
+//     console.log(`siz nafaqa yoshdasiz`)
+// } else if (myName === yosh)
+//     console.log(`teng ${yosh-myName}`)
+// else {
+//     console.log(`qancha yil bor ${yosh-myName}`)
+// }
+
+
+
+
+
+// const myAge = +prompt('Tugilgan yilingizni kiriting')
+// const year = 2023;
+// if (myAge >= 2000) {
+//     console.log(`21 asrda`)
+// } else {
+//     (myAge < 2000)
+//     console.log('20 asirda')
+// }
+
+
+
+// const myAge = +prompt('sonni kiriting');
+// const myAge2 = +prompt('sonni kiriting');
+// let myAge3 = +prompt('sonni kiriting');
+// if ((myAge + myAge2) == myAge3) {
+//     console.log(`togri`)
+// } else {
+//     console.log('notugri')
+// }
+
+
+
+
+
+// const uzsum = +prompt('sonni kiriting')
+// const moneys = 500;
+// const hotel = 250;
+// const museum = 120;
+// let ticket = 500;
+// const uzsomdollr = 11000;
+// const uzsomyevro = 12000;
+// let moneys = +prompt('sonni kiriting')
+// let holidayprice = ((hotel + ticket) * uzsomdollr) + (museum * uzsomyevro);
+// if (moneys > holidayprice) {
+//     console.log("oq yol");
+// } else {
+//     console.log("Alisher ozgina sabr qilish kerak bo’lar ekan.");
+// }
+
+
+
+
+// let isDriving = 'true';
+// let isVisible = 'true';
+
+// if (isDriving && isVisible) {
+//     console.log('mashina hayday olasiz')
+// } else {
+//     console.log('hayday olmaysiz')
+// }
+
+
+
+
+//  const myWeight1 = +prompt('Jhonni vaznini  kiritinng');
+//  const myTall1 = +prompt('Jhonni buyini kiriting');
+//  const myWeight2 = +prompt('Bobni vaznini  kiritinng');
+//  const myTall2 = +prompt('Bobni buyini  kiritinng');
+//  const Jhon = myWeight1 / myTall1 ** 2;
+//  const Bob = myWeight2 / myTall2 ** 2;
+// if (Jhon > Bob) {
+//      console.log('Jhon buyi uzun')
+
+//  } else if (Jhon === Bob) {
+//      console.log('buyi teng')
+//  } else {
+//      console.log('Bobni buyi uzun')
+//  }
+
+
+
+// one
+
+// let son = +prompt('son kiriting')
+// if (son > 0) {
+//     alert(son + 1)
+// } else {
+//     alert(son)
+// }
+
+
+
+//   two
+// let son = +prompt('son kiriting')
+// if (son > 0) {
+//     alert(son + 1)
+// } else {
+//     alert(son - 2)
+// }
+
+
+
+//  three
+// let son = +prompt('son kiriting')
+// if (son > 0) {
+//     alert(son + 1)
+// } else if (son === 0) {
+//     alert(10)
+// } else {
+//     alert(son - 2)
+// }
+
+
+
+
+// counter = 0;
+// let son1 = +prompt('1 chi sonni kiriting')
+// if (son1 > 0) {
+//     counter++
+// }
+
+// let son2 = +prompt('2 chi sonni kiriting')
+// if (son2 > 0) {
+//     counter++
+// }
+// let son3 = +prompt('3 chi sonni kiriting')
+// if (son3 > 0) {
+//     counter++
+// }
+// alert(counter);
